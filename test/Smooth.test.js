33feb39af973cb4a545c43c8e2e876ed2a944b04
Thomas van Dam <t.vandam@wearetriple.com>
2017-08-07 13:47:28 +0200

    Add CI scripts and tokens

---
+++ test/Smooth.test.js
@@ -0,0 +1,26 @@
+import '@meisterplayer/meister-mock';
+import Smooth from '../src/js/Smooth';
+
+jest.mock('../src/js/lib/hasplayer');
+
+const PLUGIN_NAME = 'Smooth';
+const SUPPORTED_TYPES = ['smooth', 'mss'];
+
+describe('Smooth class', () => {
+    test(`pluginName should be ${PLUGIN_NAME}`, () => {
+        expect(Smooth.pluginName).toBe(PLUGIN_NAME);
+    });
+
+    test('pluginVersion should return a version string', () => {
+        // Version should match the SemVer pattern (e.g. 2.11.9)
+        expect(Smooth.pluginVersion).toMatch(/\d+\.\d+\.\d+/);
+    });
+});
+
+describe('The rest of the test suite', () => {
+    test('It should be written', () => {
+        const test = { testsWritten: false };
+
+        expect(test).toEqual({ testsWritten: true });
+    });
+});
